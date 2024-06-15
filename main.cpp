6978f714f7f7901d1aef37ec3b8ffc8bbac1b33c
Burkee504 <oisburke1@gmail.com>
2024-06-15 10:10:04 +0000

    Commit final

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include "mbed.h"
 
+// A few words 
+InterruptIn bluebtn(BUTTON1);
+
+volatile int int_trig=0;
+
+void bttnFall(){
+    int_trig = 1;  
+}
+
 // main() runs in its own thread in the OS
 int main()
 {
-    while (true) {
+    bluebtn.fall(&bttnFall);
+
+// 
+while (true) {
+    if(int_trig == 1){
+        int_trig = 0;
+        printf("bttnfall \r\n");
+    }
+    
+    
+}
 
     }
-}
+
 
