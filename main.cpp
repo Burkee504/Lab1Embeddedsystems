f9e468b51502ee764b0eeb3a0f0e02ccd42cab47
Burkee504 <oisburke1@gmail.com>
2024-08-01 18:52:06 +0000

    Commit Final comments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "mbed.h"
 
-// A few words 
+// InterruptIn is used to set up digital I/O Pins as an input.This program prints "bttnfall" when the blue push button is pressed using a printf statment.  
+// While statment allows the statment included in the if to be repeated when the conditions are true. 
 InterruptIn bluebtn(BUTTON1);
 
 volatile int int_trig=0;
@@ -9,12 +10,10 @@ void bttnFall(){
     int_trig = 1;  
 }
 
-// main() runs in its own thread in the OS
 int main()
 {
     bluebtn.fall(&bttnFall);
 
-// 
 while (true) {
     if(int_trig == 1){
         int_trig = 0;
